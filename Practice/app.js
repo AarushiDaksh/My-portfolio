7aebbd4a52498c651eab075f7b5092ef9c555942
Aarushi Daksh <136158651+AarushiDaksh@users.noreply.github.com>
2024-06-15 12:29:18 +0530

    committed

--- Practice/app.js
+++ Practice/app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose'); // Import Mongoose
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
-
-// Middleware to parse URL-encoded bodies
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Set the view engine to EJS
 app.set('view engine', 'ejs');
 
-// Connect to MongoDB
+
 mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -24,33 +20,31 @@ mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true
     console.error('Error connecting to MongoDB:', error);
   });
 
-// Define a schema
+
 const contactSchema = new mongoose.Schema({
   name: String,
   message: String
 });
 
-// Create a model
+
 const Contact = mongoose.model('Contact', contactSchema);
 
-// Route to render the index.ejs file
+
 app.get('/', (req, res) => {
   res.render('contact');
 });
 
-// Route to handle form submission
+
 app.post('/contact', async (req, res) => {
   const { name, message } = req.body;
   console.log(`Name: ${name}, Message: ${message}`);
 
-  // Create a new contact document
   const newContact = new Contact({
     name: name,
     message: message
   });
 
   try {
-    // Save the contact to the database
     await newContact.save();
     console.log('Contact saved successfully');
     res.send('Form submitted successfully');
